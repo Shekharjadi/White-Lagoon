f4fd493201098b456df9a7f9b7dd411ebebe37a2
Mande P <mande20252025@outlook.com>
2025-12-04 11:49:53 +0000

    initial

---
+++ White-Lagoon/White-Lagoon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace White_Lagoon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
